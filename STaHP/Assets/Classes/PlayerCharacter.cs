8b86a8aec9ad4e0adc6d9fd6d550b913aca045ad
Mitchell Alfaro <Riddermark75@hotmail.com>
2018-06-14 22:47:34 -0400

    Throwable Birbs

---
+++ STaHP/Assets/Classes/PlayerCharacter.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+
+    public int[] distanceToThrow;
+    public int[] bombToThrow;
+    public GameObject throwable;
+    public Transform throwOrigin;
+    public Vector2 throwOriginVec;
+    public Vector2 throwSpeed;
+    public float gravity;
+    //public Transform throwPointPos;
+    public Vector2 throwDestination;
+    public float flightSpeed;
+    public float throwTimer;
+    public float throwResetTimer;
+    public float landingTime;
+    public Vector2 clickPos;
+
+    public ThrowableController thrownObject;
+    public Rigidbody2D thrownRB;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (throwTimer > 0)
+        {
+            throwTimer -= Time.deltaTime;
+        }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Click");
+            clickPos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            Debug.Log(clickPos);
+            InstantOrigin();
+            Vector2 throwDestination = new Vector2(clickPos.x - throwOriginVec.x, clickPos.y - throwOriginVec.y);
+            throwDestination.Normalize();
+            Debug.Log("Throw destination is " + throwDestination);
+            Throw(throwDestination);
+        }
+    }
+
+    public void InstantOrigin()
+    {
+        throwOriginVec = Camera.main.WorldToViewportPoint(throwOrigin.position);
+        //throwOriginVec = new Vector2(throwOrigin.position.x, throwOrigin.position.y);
+    }
+
+    public void Throw(Vector2 throwDestination)
+    {
+        if (throwTimer > 0)
+        {
+            Debug.Log("Cooling down.");
+            return;
+        }
+
+        throwTimer = throwResetTimer;
+        Debug.Log("Throwing bomb to " + throwDestination);
+        //Instantiate(bomb, throwOrigin.position, throwOrigin.rotation);
+        GameObject newThrowable = Instantiate(throwable, throwOrigin.position, throwOrigin.rotation);
+        thrownObject = newThrowable.GetComponent<ThrowableController>();
+        thrownRB = newThrowable.GetComponent<Rigidbody2D>();
+        newThrowable.GetComponent<ThrowableController>().throwableRigidbody = newThrowable.GetComponent<Rigidbody2D>();
+        newThrowable.GetComponent<ThrowableController>().SetThrowDestination(throwDestination);
+    }
+}
---
+++ STaHP/Assets/Classes/ThrowableController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowableController : MonoBehaviour
+{
+
+    public int bombTimer;
+    public float explosionRadius;
+    public int resetTimer;
+    public bool exploded;
+    public Animation anim;
+    public Rigidbody2D throwableRigidbody;
+    public float throwSpeed;
+    public float gravity;
+
+    public bool initFlight = false;
+
+    private PlayerCharacter thePlayer;
+
+    // Use this for initialization
+    void Start()
+    {
+        throwableRigidbody = GetComponent<Rigidbody2D>();
+        thePlayer = FindObjectOfType<PlayerCharacter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.right = throwableRigidbody.velocity - new Vector2(transform.position.x, transform.position.y);
+
+        //transform.rotation = Quaternion.LookRotation(throwableRigidbody.velocity);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Surface")
+        {
+            throwableRigidbody.velocity = new Vector2(0, 0);
+        }
+    }
+
+    public void SetThrowDestination(Vector2 throwDestination)
+    {
+        throwableRigidbody.AddForce(throwDestination * throwSpeed);
+        Debug.Log("We Goin " + throwDestination);
+    }
+}
